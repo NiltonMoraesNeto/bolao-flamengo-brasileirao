6afc6460d5e1b8cf866a0fc2a4ae2552b0f65b1d
Nilton Moraes Neto <nilton.nmn@gmail.com>
2025-03-20 20:09:51 -0300

    configuracoes de deploy vercel

--- src/services/json-server.js
+++ src/services/json-server.js
@@ -1,10 +1,16 @@
-const jsonServer = require('json-server');
-const path = require('path');
+import jsonServer from 'json-server';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Esses dois comandos são necessários para obter o caminho do arquivo em um módulo ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '..', '..', 'db.json'));
+const router = jsonServer.router(path.join(__dirname, '..', '..', 'db.json')); // Caminho para o arquivo db.json na raiz do projeto
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = server;
+export default server;
