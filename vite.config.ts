eb24d498e976fd564bef4d36b3d90a3c33c9e16d
Nilton Moraes Neto <nilton.nmn@gmail.com>
2025-03-21 14:11:34 -0300

    configuracoes de deploy vercel

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  //base: "/bolao-flamengo-brasileirao/",
+  base: "/bolao-flamengo-brasileirao/",
   test: {
     globals: true,
     environment: "jsdom"
